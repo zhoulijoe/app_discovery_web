a27e4769190e6385fa60a75d24b028cfa6cb9831
Zhou Li <zhoulijoe@gmail.com>
2015-08-08 23:55:30 -0700

    Create angularjs project using yeoman generator

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appDiscoveryWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the appDiscoveryWebApp
+ */
+angular.module('appDiscoveryWebApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appDiscoveryWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the appDiscoveryWebApp
+ */
+angular.module('appDiscoveryWebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
